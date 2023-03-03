8585d21ee60aac4afb2411e2088f9c3110c6e7ff
Mario Chavez <mariochavez1292@gmail.com>
2023-03-03 07:26:58 -0600

    updates

--- scripts/fibonacchi.py
+++ scripts/fibonacchi.py
@@ -1,6 +1,8 @@
 
 def Fibonacci(n: int) -> int:
-    if n in [0, 1]:
+    if n < 0:
+        raise ValueError('Fibonacci function not defined for n < 0')
+    elif n in [0, 1]:
         return n
     else:
         return Fibonacci(n - 1) + Fibonacci(n - 2)
@@ -9,3 +11,4 @@ if __name__ == '__main__':
     for i in range(11):
         print(Fibonacci(i), end= ' ')
     print(' ')
+    print(Fibonacci(-22))
